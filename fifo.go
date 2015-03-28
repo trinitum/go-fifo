cb9eb95c3452d9dbe0615d664f3d682286197b6e
Pavel Shaydo <zwon@trinitum.org>
2015-03-28 23:44:53 +0100

    add documentation

--- fifo.go
+++ fifo.go
@@ -1,3 +1,6 @@
+// Package fifo implements FIFO buffer. You can push items into buffer,
+// shift items from it, or you can read arbitrary items in the buffer.
+// Buffer automatically grows when required.
 package fifo
 
 type FIFO struct {
@@ -6,12 +9,14 @@ type FIFO struct {
 	length int
 }
 
+// New creates a new buffer and preallocates storage of the specified size.
 func New(size int) *FIFO {
 	return &FIFO{
 		items: make([]interface{}, size),
 	}
 }
 
+// Push adds a new item to the end of the buffer.
 func (buf *FIFO) Push(item interface{}) {
 	bufSize := len(buf.items)
 
@@ -44,6 +49,8 @@ func (buf *FIFO) Push(item interface{}) {
 	buf.length++
 }
 
+// Shift removes the first item from the buffer.
+// It may return nil if the buffer is empty.
 func (buf *FIFO) Shift() interface{} {
 	if buf.length == 0 {
 		return nil
@@ -54,6 +61,8 @@ func (buf *FIFO) Shift() interface{} {
 	return item
 }
 
+// Item returns specified item from the buffer.
+// If the index is out of range it returns nil.
 func (buf *FIFO) Item(idx int) interface{} {
 	if idx >= buf.length {
 		return nil
@@ -61,6 +70,7 @@ func (buf *FIFO) Item(idx int) interface{} {
 	return buf.items[(buf.start+idx)%len(buf.items)]
 }
 
+// Len returns the number of items in the buffer.
 func (buf *FIFO) Len() int {
 	return buf.length
 }
